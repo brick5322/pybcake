0a85342e0876da277437dc84458eb84495100b07
福百川 刘 <brick5322@outlook.com>
2023-01-07 10:50:00 +0800

    add comment

--- bcake.py
+++ bcake.py
@@ -1,11 +1,16 @@
-import pybcake as pbm
+import pybcake as pbc
 import os
 
+
 def release():
-    bcake = pbm.gcc_exe(["bcakepy.cpp"],"bcake",target_dir = "./deb_dir/usr/bin",mode="release",include_dirs = ["/usr/include/python3.9"],libs = ["python3.9"])
+    bcake = pbc.gcc_exe(["bcakepy.cpp"], "bcake", target_dir="./deb_dir/usr/bin", mode="release",
+                        include_dirs=["/usr/include/python3.9"], libs=["python3.9"])
     bcake.make()
     bcake.clean(reserve_target=True)
     os.system("mkdir -p deb_dir/usr/lib/python3/dist-packages")
     os.system("cp -r ./pybcake deb_dir/usr/lib/python3/dist-packages")
     os.system("dpkg -b ./deb_dir .")
 
+# before you make and install ,uncommit this to make
+# after that, you can commit this to test
+# release()
